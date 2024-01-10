49a80921e47dac223b14f56f2f410d1e69adf55e
Nelson Durrant <snelsondurrant@gmail.com>
2024-01-10 22:55:45 +0000

    Updates for debugging

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ cd ~/teensy_ws/sensors
 pio run
 
 cd .pio/build/teensy41
-teensy_loader_cli --mcu=TEENSY41 -sv firmware.hex
-# teensy_loader_cli --mcu=TEENSY41 -sv firmware.hex
+teensy_loader_cli --mcu=TEENSY41 -s -v firmware.hex
+# teensy_loader_cli --mcu=TEENSY41 -s -v firmware.hex
