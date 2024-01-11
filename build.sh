bbe5697a9e0b5dfd2450d48f73f62ad6daaa6424
Nelson Durrant <snelsondurrant@gmail.com>
2024-01-11 00:33:39 +0000

    Tried tycmd -- still same upload error

--- build.sh
+++ build.sh
@@ -4,5 +4,4 @@ cd ~/teensy_ws/sensors
 pio run
 
 cd .pio/build/teensy41
-teensy_loader_cli --mcu=TEENSY41 -s -v firmware.hex
-# teensy_loader_cli --mcu=TEENSY41 -s -v firmware.hex
+tycmd upload firmware.hex
--- upload.sh
+++ upload.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd ~/teensy_ws/sensors/.pio/build/teensy41
-teensy_loader_cli --mcu=TEENSY41 -s -v firmware.hex
+tycmd upload firmware.hex
