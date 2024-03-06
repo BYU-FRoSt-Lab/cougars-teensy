abc1af0834a59d7958c4e41c682e90df4bd2de8d
Nelson Durrant <snelsondurrant@gmail.com>
2024-03-06 20:22:24 +0000

    Added some documentation

--- init.sh
+++ init.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Crontab runs this script on boot! Be very careful editing it.
+# Run-time errors are logged to ~/logs/crontab
+
 cd /
 cd home/frostlab/teensy_ws/gpio
 sudo python3 init_states.py
