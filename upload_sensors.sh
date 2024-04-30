eaa8f2aadc6418617b88fb5fb4d6db08382a3cd8
Nelson Durrant <107496339+snelson-durrant@users.noreply.github.com>
2024-04-30 15:50:26 -0600

    Update upload_sensors.sh

--- upload_sensors.sh
+++ upload_sensors.sh
@@ -2,12 +2,16 @@
 
 cd ~/teensy_ws/gpio
 python3 toggle_on_sensors.py
+python3 toggle_off_control.py
 python3 program_sensors.py
 
 cd ~/teensy_ws/sensors/.pio/build/teensy41
 # cd ~/teensy_ws/test_ros/.pio/build/teensy41
 # cd ~/teensy_ws/test/.pio/build/teensy41
 timeout 10s tycmd upload firmware.hex --board "13441690"
+
+cd ~/teensy_ws/gpio
+python3 toggle_on_control.py
 
 exit_status=$?
 if [[ $exit_status -eq 124 ]]; 
