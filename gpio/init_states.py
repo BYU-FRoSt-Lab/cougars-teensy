8d95bd908a5b0001b770854374230f340c4c6492
Nelson Durrant <107496339+snelson-durrant@users.noreply.github.com>
2024-04-30 12:28:32 -0600

    Update init_states.py

--- gpio/init_states.py
+++ gpio/init_states.py
@@ -1,7 +1,8 @@
 # Crontab runs this script on boot! Be very careful editing it.
 # Run-time errors are logged to ~/logs/crontab
+# THE ABOVE MIGHT BE OUTDATED
 
-import lgpio
+import gpiod
 
 CONTROL_PIN = 27
 SENSORS_PIN = 22
@@ -9,17 +10,22 @@ CONTROL_PROGRAM_PIN = 24
 SENSORS_PROGRAM_PIN = 21
 
 # open the GPIO chip
-h = lgpio.gpiochip_open(0)
-lgpio.gpio_claim_output(h, CONTROL_PIN)
-lgpio.gpio_claim_output(h, SENSORS_PIN)
-lgpio.gpio_claim_output(h, CONTROL_PROGRAM_PIN)
-lgpio.gpio_claim_output(h, SENSORS_PROGRAM_PIN)
+chip = gpiod.Chip('gpiochip4')
+control_line = chip.get_line(CONTROL_PIN)
+sensors_line = chip.get_line(SENSORS_PIN)
+control_prog_line = chip.get_line(CONTROL_PROGRAM_PIN)
+sensors_prog_line = chip.get_line(SENSORS_PROGRAM_PIN)
+
+control_line.request(consumer="CONTROL", type=gpiod.LINE_REQ_DIR_OUT)
+sensors_line.request(consumer="SENSORS", type=gpiod.LINE_REQ_DIR_OUT)
+control_prog_line.request(consumer="CONTROL_PROG", type=gpiod.LINE_REQ_DIR_OUT)
+sensors_prog_line.request(consumer="SENSORS_PROG", type=gpiod.LINE_REQ_DIR_OUT)
 
 # set the GPIO pins high
-lgpio.gpio_write(h, CONTROL_PIN, 1)
-lgpio.gpio_write(h, SENSORS_PIN, 1)
-lgpio.gpio_write(h, CONTROL_PROGRAM_PIN, 1)
-lgpio.gpio_write(h, SENSORS_PROGRAM_PIN, 1)
+control_line.set_value(1)
+sensors_line.set_value(1)
+control_prog_line.set_value(1)
+sensors_prog_line.set_value(1)
 
 # set the .txt files to start OFF
 with open('sensors_state.txt', 'w') as f:
