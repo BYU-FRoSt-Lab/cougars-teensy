2781ab7ff8f0093c800886f6f7f66cbbaa7a8312
Brighton Anderson <bja1701@byu.edu>
2024-06-13 13:48:49 -0600

    refactor

--- gpio/strobe_ctrl.py
+++ gpio/strobe_ctrl.py
@@ -1,23 +1,17 @@
 import gpiod
 import time
-# import sys
-# import threading
-
 LED_PIN = 15 #RX pin of pi
 chip = gpiod.Chip('gpiochip4')
 led_line = chip.get_line(LED_PIN)
 led_line.request(consumer="LED", type=gpiod.LINE_REQ_DIR_OUT)
-
-toggle = 1
-
-while toggle == 1:
-    try:
-        led_line.set_value(1)
-        time.sleep(1)
-        led_line.set_value(0)
-        time.sleep(1)
-    finally:
-        led_line.release()
+try:
+   while True:
+       led_line.set_value(1)
+       time.sleep(1)
+       led_line.set_value(0)
+       time.sleep(1)
+finally:
+   led_line.release()
 
 
 
