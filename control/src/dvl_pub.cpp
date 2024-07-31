ea1defc8460f2de3af6e7e98df9e2a72e7b8d4f1
Nelson Durrant <snelsondurrant@gmail.com>
2024-07-31 14:47:24 -0600

    testing strings

--- control/src/dvl_pub.cpp
+++ control/src/dvl_pub.cpp
@@ -10,14 +10,9 @@ void DVLPub::setup(rcl_node_t node) {
 void DVLPub::publish(String wrz, String wrp, String wru) {
 
   // TODO: This compiles, but is still broken for some reason
-  // sprintf(msg.wrz.data, wrz.c_str());
-  // msg.wrz.size = strlen(msg.wrz.data);
-
-  // sprintf(msg.wrp.data, wrp.c_str());
-  // msg.wrp.size = strlen(msg.wrp.data);
-
-  // sprintf(msg.wru.data, wru.c_str());
-  // msg.wru.size = strlen(msg.wru.data);
+  msg.wrz = wrz;
+  msg.wrp = wrp;
+  msg.wru = wru;
 
   msg.header.stamp.nanosec = rmw_uros_epoch_nanos();
   RCSOFTCHECK(rcl_publish(&publisher, &msg, NULL));
