5d43afae0b6458a33467c0ba0e3ec3317aa3ee0e
Nelson Durrant <snelsondurrant@gmail.com>
2024-08-30 10:57:35 -0600

    Updates

--- init.sh
+++ init.sh
@@ -9,8 +9,6 @@
 # Check if the teensy power states are already set
 if [ -z "$(tycmd list)" ]; then
 
-    cd /
     cd home/frostlab/teensy_ws/gpio_tools
     sudo python3 init_states.py
-    cd /
 fi
