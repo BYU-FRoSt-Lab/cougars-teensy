9f150496909b807179a6dc177658fb9b156659ca
Nelson Durrant <snelsondurrant@gmail.com>
2024-08-30 10:52:22 -0600

    Documentation updates

--- gpio_tools/strobe_on.py
+++ gpio_tools/strobe_on.py
@@ -12,6 +12,8 @@ chip = gpiod.Chip('/dev/gpiochip4')
 strobe_line = chip.get_line(STROBE_PIN)
 strobe_line.request(consumer="STROBE", type=gpiod.LINE_REQ_DIR_OUT)
 
+print("COMPLETE: Strobe light is now on")
+
 try:
     while True:
         strobe_line.set_value(1)
@@ -19,6 +21,4 @@ try:
         strobe_line.set_value(0)
         time.sleep(STROBE_RATE)
 finally:
-    print("COMPLETE: Strobe light is now off")
-    strobe_line.set_value(0)
     strobe_line.release()
