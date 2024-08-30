3cd4e0d22dc859eca4a4f3c5ce4309ad4d03d8b7
Nelson Durrant <snelsondurrant@gmail.com>
2024-08-30 10:48:56 -0600

    Updates

--- gpio_tools/strobe_off.py
+++ gpio_tools/strobe_off.py
@@ -10,6 +10,8 @@ from gpio_def import *
 chip = gpiod.Chip('/dev/gpiochip4')
 strobe_line = chip.get_line(STROBE_PIN)
 strobe_line.request(consumer="STROBE", type=gpiod.LINE_REQ_DIR_OUT)
+
+print("COMPLETE: Strobe light is now off")
         
 strobe_line.set_value(0)
 strobe_line.release()
