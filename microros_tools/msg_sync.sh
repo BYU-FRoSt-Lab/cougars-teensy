0c8eaf1e97b7d2b3a7cb0fae48986d487b3fc578
Nelson Durrant <snelsondurrant@gmail.com>
2024-10-11 09:04:24 -0600

    Combined msg_update and msg_sync

--- microros_tools/msg_sync.sh
+++ microros_tools/msg_sync.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 # Created by Nelson Durrant, Sep 2024
 #
-# Syncs frost_interfaces changes with the CougarsTeensy repo
-# - After running this script, recompile micro-ROS in 
-#   PlatformIO using 'msg_update.sh'
+# Syncs frost_interfaces changes with the cougars-teensy repo
+# and recompiles the micro-ROS library
 
 rsync -avc --delete ~/ros2_ws/src/frost_interfaces ~/teensy_ws/cougars/extra_packages
+
+cd ~/teensy_ws/cougars
+pio run --target clean_microros
+pio lib install
+pio run
