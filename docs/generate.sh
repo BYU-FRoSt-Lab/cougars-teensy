cf504990f05cbb11f2bb4914bb5da58c61949b94
Nelson Durrant <snelsondurrant@gmail.com>
2024-10-11 12:01:29 -0600

    Documentation working

---
+++ docs/generate.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Created by Nelson Durrant, Oct 2024
+#
+# Generates documentation for the micro-ROS node
+# - The documentation can be accessed by opening the 'index.html'
+#   file in the 'build/html' directory
+
+make html
