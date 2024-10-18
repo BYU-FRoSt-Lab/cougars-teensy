e2ea230c493a2ec41838711e098eb42af1aaf6c3
BradenMeyers <bjz-github@spwiz.com>
2024-10-18 10:10:16 -0600

    Updates to tests

--- microros_tools/microros_test.sh
+++ microros_tools/microros_test.sh
@@ -33,6 +33,12 @@ cleanup() {
 }
 trap cleanup SIGINT
 
+# Quick fix for daemon error (TODO: find a better solution)
+source ~/ros2_ws/install/setup.bash
+ros2 daemon stop
+ros2 daemon start
+sleep 5
+
 bash ~/gpio/power.sh on
 
 if [ -z "$(tycmd list | grep Teensy)" ]; then
@@ -49,25 +55,30 @@ source ~/ros2_ws/install/setup.bash
 
 echo ""
 
+# TODO: Test this implementation with a string
 leak_data=$(timeout 3 ros2 topic echo --once --no-arr $NAMESPACE/leak/data 2>/dev/null | grep -oP '(?<=leak: )\d+')
 if [ -z "$leak_data" ]; then
   printFailure "No leak sensor connection found."
 else
   if [[ $(echo "$leak_data" | awk '{if ($1 == false) print 1; else print 0}') -eq 0 ]]; then
-    printSuccess "Leak sensor connected! (leak: $leak_data)"
+    printSuccess "Leak sensor connected!"
+    echo "  leak: $leak_data"
   else
-    printFailure "Leak sensor may not be working. (leak: $leak_data)"
+    printFailure "Leak sensor may not be working."
+    echo "  leak: $leak_data"
   fi
 fi
 
-battery_data=$(timeout 3 ros2 topic echo --once --no-arr $NAMESPACE/battery/data 2>/dev/null | grep -oP '(?<=voltage: )\d+')
+battery_data=$(timeout 3 ros2 topic echo --once --no-arr $NAMESPACE/battery/data 2>/dev/null | grep -oP '(?<=voltage: )\d+(\.\d+)?')
 if [ -z "$battery_data" ]; then
   printFailure "No battery monitor connection found."
 else
   if [[ $(echo "$battery_data" | awk '{if ($1 == 0.0) print 1; else print 0}') -eq 0 ]]; then
-    printSuccess "Battery monitor connected! (voltage: $battery_data)"
+    printSuccess "Battery monitor connected!"
+    echo "  voltage: $battery_data"
   else
-    printFailure "Battery monitor may not be working. (voltage: $battery_data)"
+    printFailure "Battery monitor may not be working."
+    echo "  voltage: $battery_data"
   fi
 fi
 
@@ -76,11 +87,14 @@ if [ -z "$pressure_data" ]; then
   printFailure "No pressure sensor connection found."
 else
   if [[ $(echo "$pressure_data" | awk '{if ($1 == 0.0) print 1; else print 0}') -eq 0 ]]; then
-    printSuccess "Pressure sensor connected! (fluid_pressure: $pressure_data)"
+    printSuccess "Pressure sensor connected!"
+    echo "  fluid_pressure: $pressure_data"
   else
-    printFailure "Pressure sensor may not be working. (fluid_pressure: $pressure_data)"
+    printFailure "Pressure sensor may not be working."
+    echo "  fluid_pressure: $pressure_data"
   fi
 fi
+
 
 echo ""
 
