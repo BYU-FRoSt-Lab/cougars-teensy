ee4622c7703381e795e47e2aac86a5e6a0712cfe
Nelson Durrant <snelsondurrant@gmail.com>
2024-10-30 18:30:16 -0600

    Documenation updates

--- cougars/include/namespace.h
+++ cougars/include/namespace.h
@@ -2,6 +2,8 @@
  * Simple namespace declaration for micro-ROS. This is used to avoid naming
  * conflicts with the other vehicles while still keeping them all under the 
  * same ROS_DOMAIN_ID. THIS NEEDS TO MATCH THE ROS 2 NAMESPACE!
+ * 
+ * TODO: Figure out a better implementation for this.
  */
 
 #define NAMESPACE "coug1"
