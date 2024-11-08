0f5c1a80e8dfc3433930cb3f4c83116aa911b5f3
Nelson Durrant <snelsondurrant@gmail.com>
2024-11-08 13:40:00 -0700

    Documentation updates

--- cougars/include/namespace.h
+++ cougars/include/namespace.h
@@ -3,7 +3,7 @@
  * conflicts with the other vehicles while still keeping them all under the 
  * same ROS_DOMAIN_ID. THIS NEEDS TO MATCH THE ROS 2 NAMESPACE!
  * 
- * TODO: Figure out a better implementation for this.
+ * TODO: Figure out how to add this to the config folder.
  */
 
 #define NAMESPACE "coug1"
