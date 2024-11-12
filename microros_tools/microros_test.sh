6abdaa1892555cc3ef48772ed9c99fbb681b73a1
Nelson Durrant <snelsondurrant@gmail.com>
2024-11-11 17:25:41 -0700

    Coloring fixes

--- microros_tools/microros_test.sh
+++ microros_tools/microros_test.sh
@@ -8,23 +8,23 @@
 source ~/config/bash_vars.sh
 
 function printInfo {
-  echo -e "\033[0m\033[96m[INFO] $1\033[0m"
+  echo -e "\033[0m\033[36m[INFO] $1\033[0m"
 }
 
 function printWarning {
-  echo -e "\033[0m\033[93m[WARNING] $1\033[0m"
+  echo -e "\033[0m\033[33m[WARNING] $1\033[0m"
 }
 
 function printError {
-  echo -e "\033[0m\033[91m[ERROR] $1\033[0m"
+  echo -e "\033[0m\033[31m[ERROR] $1\033[0m"
 }
 
 function printSuccess {
-  echo -e "\033[0m\033[92m[SUCCESS] $1\033[0m"
+  echo -e "\033[0m\033[32m[SUCCESS] $1\033[0m"
 }
 
 function printFailure {
-  echo -e "\033[0m\033[91m[FAIL] $1\033[0m"
+  echo -e "\033[0m\033[31m[FAIL] $1\033[0m"
 }
 
 cleanup() {
