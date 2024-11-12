ca4397113f789ae6fb29c40029d126b27a858838
Nelson Durrant <snelsondurrant@gmail.com>
2024-11-11 17:27:32 -0700

    Quick microros fix while I remember

--- microros_tools/microros_test.sh
+++ microros_tools/microros_test.sh
@@ -99,4 +99,6 @@ timeout 5 ros2 topic pub -1 $NAMESPACE/kinematics/command frost_interfaces/msg/U
 printInfo "Testing thruster (OFF), publishing to 'kinematics/command'..."
 timeout 5 ros2 topic pub -1 $NAMESPACE/kinematics/command frost_interfaces/msg/UCommand '{fin: [0, 0, 0, 0], thruster: 0}' 2>/dev/null
 
+echo ""
+
 cleanup
